988f0961b13f8e6716d14b1fd734daed5726fed9
Renyi03 <yinyeji@gmail.com>
2025-03-03 11:37:12 +0100

    screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,11 @@
 #include "raylib.h"
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
 int main(void)
 {   
     Color blue = { 117, 160, 210, 150 };
-    int ballX = 400;
-    int ballY = 400;
     InitWindow(800, 800, "El mejor juego de proyecto 1: An indescribable emptiness");
     SetTargetFPS(60);
 
-    //Game Loop
-
-    while (WindowShouldClose() == false) {
-
-        // 1. Event handling
-        if (IsKeyDown(KEY_RIGHT)) {
-            ballX += 3;
-        }
-
-        else if (IsKeyDown(KEY_LEFT)) {
-            ballX -= 3;
-        }
-
-        else if (IsKeyDown(KEY_UP)) {
-            ballY -= 3;
-        }
-
-        else if (IsKeyDown(KEY_DOWN)) {
-            ballY += 3;
-        }
-    
-        // 2. Update positions
-        
-    
-        // 3. Drawing
-        BeginDrawing();
-        ClearBackground(blue);
-        DrawCircle(ballX, ballY, 20, YELLOW);
-
-        EndDrawing();
-    }
 
     CloseWindow();
     
