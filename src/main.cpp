acda98cd50fef2f71c99f2f58abd01765a844a66
Valkyn22 <clara.sanchez.via@gmail.com>
2025-03-03 11:51:28 +0100

    prueba

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,47 @@
 #include "raylib.h"
 
 int main(void)
-{   
-    Color blue = { 117, 160, 210, 150 };
+{
+    int ballX = 400;
+    int ballY = 400;
     InitWindow(800, 800, "El mejor juego de proyecto 1: An indescribable emptiness");
     SetTargetFPS(60);
 
+    //Game Loop
+
     while (WindowShouldClose() == false) {
 
-       
+        // 1. Event handling
+        if (IsKeyDown(KEY_RIGHT)) {
+            ballX += 3;
+        }
 
+        else if (IsKeyDown(KEY_LEFT)) {
+            ballX -= 3;
+        }
+
+        else if (IsKeyDown(KEY_UP)) {
+            ballY -= 3;
+        }
+
+        else if (IsKeyDown(KEY_DOWN)) {
+            ballY += 3;
+        }
+
+        // 2. Update positions
+
+
+        // 3. Drawing
         BeginDrawing();
-        
+        ClearBackground(BLACK);
+        DrawCircle(ballX, ballY, 20, RED);
 
         EndDrawing();
-
     }
 
     CloseWindow();
-    
+
 
     return 0;
 }
+
