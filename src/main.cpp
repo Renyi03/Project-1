00cd06fe254f8f2eccd99c32951e4f78f3c35f2b
Renyi03 <yinyeji@gmail.com>
2025-03-24 12:47:27 +0100

    initial + title screen

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include "Pengo.hpp"
 #include "Map.hpp"
 
+typedef enum GameScreen { INITIAL, TITLE, GAMEPLAY, POINTS } GameScreen;
+
 int main(void)
 {
     InitWindow(800, 800, "El mejor juego de proyecto 1: An indescribable emptiness");
@@ -16,23 +18,73 @@ int main(void)
     Pengo pengo{border};
     Map map{};
 
+    
+    GameScreen currentScreen = INITIAL;
+
     //Game Loop
 
     while (WindowShouldClose() == false) {
 
-        pengo.Update();
-        bool isColliding = CheckCollisionRecs(pengo.GetRect(), borderTop);
+        switch (currentScreen) {
+
+        case INITIAL:
+        {
+            if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
+            {
+                currentScreen = TITLE;
+            }
+        } break;
+        case TITLE:
+        {
+            if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
+            {
+                currentScreen = GAMEPLAY;
+            }
+        } break;
+        case GAMEPLAY:
+        {
+            pengo.Update();
+            bool isColliding = CheckCollisionRecs(pengo.GetRect(), borderTop);
+            BeginDrawing();
+            ClearBackground(BLACK);
+
+            DrawRectangleLinesEx(borderTop, 10, BLUE);
+            DrawRectangleLinesEx(borderBottom, 10, BLUE);
+            DrawRectangleLinesEx(borderLeft, 10, BLUE);
+            DrawRectangleLinesEx(borderRight, 10, BLUE);
+
+            pengo.Draw();
+            map.Draw();
+            pengo.DrawHitbox(isColliding);
+        }
+        }
+
         BeginDrawing();
+
         ClearBackground(BLACK);
 
-        DrawRectangleLinesEx(borderTop, 10, BLUE);
-        DrawRectangleLinesEx(borderBottom, 10, BLUE);
-        DrawRectangleLinesEx(borderLeft, 10, BLUE);
-        DrawRectangleLinesEx(borderRight, 10, BLUE);
+        switch (currentScreen)
+        {
+        case INITIAL:
+        {
+            // TODO: Draw LOGO screen here!
+            DrawText("PENGO PROTOTYPE", 20, 20, 40, LIGHTGRAY);
+            DrawText("WAIT for 2 SECONDS...", 290, 220, 20, GRAY);
 
-        pengo.Draw();
-        map.Draw();
-        pengo.DrawHitbox(isColliding);
+        } break;
+        case TITLE:
+        {
+            // TODO: Draw TITLE screen here!
+            DrawRectangle(0, 0, 800, 800, GREEN);
+            DrawText("TITLE SCREEN", 20, 20, 40, DARKGREEN);
+            DrawText("PRESS ENTER or TAP to JUMP to GAMEPLAY SCREEN", 120, 220, 20, DARKGREEN);
+
+        } break;
+        default: break;
+        }
+
+
+        
         
 
         EndDrawing();
